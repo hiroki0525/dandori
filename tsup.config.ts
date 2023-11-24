64a72f74524705629ff5ba9f5d10b7e72f794bf4
Hiroki Miyaji <hdekm852@gmail.com>
2023-11-24 15:31:02 +0900

    fix cli error

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,4 +5,14 @@ export default defineConfig({
   dts: true,
   format: ["esm"],
   entry: ["src", "!src/**/*.test.*", "!src/**__test__/**"],
+  platform: "node",
+  banner: {
+    // Error: "Dynamic require of "os" is not supported"
+    // Solution: https://github.com/evanw/esbuild/issues/1921#issuecomment-1623640043
+    js: `
+const require = (await import("node:module")).createRequire(import.meta.url);
+const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+const __dirname = (await import("node:path")).dirname(__filename);
+    `,
+  },
 });
